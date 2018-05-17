fd8956d32d574d3ebbf4600f426747e618dbceb2
mahmoud <mahmoud.elhussieni21@gmail.com>
2018-05-17 18:28:27 +0200

    word cloud visualization

--- DeepNN.py
+++ DeepNN.py
@@ -4,7 +4,6 @@ import numpy as np
 import re
 import collections
 import matplotlib.pyplot as plt
-%matplotlib inline
 from wordcloud import WordCloud,STOPWORDS
 # Packages for data preparation
 from sklearn.model_selection import train_test_split
@@ -18,3 +17,66 @@ from keras import models
 from keras import layers
 from keras import regularizers
 
+# Importing the dataset
+tweets_df = pd.read_csv('Tweets.csv')
+tweets_df = tweets_df[['text', 'airline_sentiment']]
+
+
+def remove_stopwords(input_text):
+    stopwords_list = stopwords.words('english')
+    # Some words which might indicate a certain sentiment are kept via a whitelist
+    whitelist = ["n't", "not", "no"]
+    words = input_text.split()
+    clean_words = [word for word in words if (word not in stopwords_list or word in whitelist) and len(word) > 1]
+    return " ".join(clean_words)
+
+
+def remove_mentions(input_text):
+    return re.sub(r'@\w+', '', input_text)
+
+
+tweets_df.text = tweets_df.text.apply(remove_stopwords).apply(remove_mentions)
+
+X_train, X_test, y_train, y_test = train_test_split(tweets_df.text, tweets_df.airline_sentiment, test_size=0.1, random_state=37)
+
+tk = Tokenizer(num_words= 10000,
+               filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
+               lower=True,
+               split=" ")
+tk.fit_on_texts(X_train)
+
+X_train_seq = tk.texts_to_sequences(X_train)
+X_test_seq = tk.texts_to_sequences(X_test)
+
+def one_hot_seq(seqs, nb_features = 10000):
+    ohs = np.zeros((len(seqs), nb_features))
+    for i, s in enumerate(seqs):
+        ohs[i, s] = 1.
+    return ohs
+
+X_train_oh = one_hot_seq(X_train_seq)
+X_test_oh = one_hot_seq(X_test_seq)
+
+le = LabelEncoder()
+y_train_le = le.fit_transform(y_train)
+y_test_le = le.transform(y_test)
+y_train_oh = to_categorical(y_train_le)
+y_test_oh = to_categorical(y_test_le)
+
+X_train_rest, X_valid, X_train_rest, y_valid = train_test_split(X_train_oh, y_train_oh, test_size=0.1, random_state=37)
+
+assert X_valid.shape[0] == y_valid.shape[0]
+assert X_train_rest.shape[0] == y_train_rest.shape[0]
+
+drop_model = models.Sequential()
+drop_model.add(layers.Dense(64, init = 'uniform', activation='relu', input_shape=(10000,)))
+drop_model.add(layers.Dropout(0.5))
+drop_model.add(layers.Dense(64,init = 'uniform', activation='relu'))
+drop_model.add(layers.Dropout(0.5))
+drop_model.add(layers.Dense(3, activation='softmax'))
+#drop_model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+print(drop_model.summary())
+
+drop_model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+drop_model.fit(X_train_rest,y_train_rest, batch_size = 64, nb_epoch = 5)
---
+++ WordCloudVisualization.py
@@ -0,0 +1,72 @@
+# Basic packages
+import pandas as pd
+import numpy as np
+import re
+import collections
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud,STOPWORDS
+
+
+# Importing the dataset
+tweets_df = pd.read_csv('Tweets.csv')
+tweets_df = tweets_df[['text', 'airline_sentiment']]
+
+
+
+df_negative = tweets_df[tweets_df['airline_sentiment']=='negative']
+words = ' '.join(df_negative['text'])
+cleaned_word = " ".join([word for word in words.split()
+                            if 'http' not in word
+                                and not word.startswith('@')
+                                and word != 'RT'
+                            ])
+wordcloud = WordCloud(stopwords=STOPWORDS,
+                      background_color='black',
+                      width=3000,
+                      height=2500
+                     ).generate(cleaned_word)
+
+
+plt.figure(1,figsize=(12, 12))
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show()
+
+#visualization using wordcloud for the positive tweets
+df_postive = tweets_df [tweets_df['airline_sentiment']=='positive']
+words = ' '.join(df_postive['text'])
+cleaned_word = " ".join([word for word in words.split()
+                            if 'http' not in word
+                                and not word.startswith('@')
+                                and word != 'RT'])
+wordcloud = WordCloud(stopwords=STOPWORDS,
+                      background_color='black',
+                      width=3000,
+                      height=2500
+                     ).generate(cleaned_word)
+
+
+plt.figure(1,figsize=(12, 12))
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show()
+
+
+
+#visualization using wordcloud for the neutral tweets
+df_neutral = tweets_df[tweets_df['airline_sentiment']=='neutral']
+words = ' '.join(df_neutral['text'])
+cleaned_word = " ".join([word for word in words.split()
+                            if 'http' not in word
+                                and not word.startswith('@')
+                                and word != 'RT'])
+wordcloud = WordCloud(stopwords=STOPWORDS,
+                      background_color='white',
+                      width=3000,
+                      height=2500
+                     ).generate(cleaned_word)
+
+plt.figure(1,figsize=(12, 12))
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show()
